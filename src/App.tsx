1e9a84b1ea596478bd97eb129b781f4ec7bd518b
Purna Shrestha <crush9918@gmail.com>
2024-10-19 21:25:53 +0545

    feat: Add the fonts and the lander

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,26 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <>
-      <main className="font-title flex h-screen items-center justify-center text-7xl text-red-500">
+      {/* <main className="font-title flex h-screen items-center justify-center text-7xl text-red-500">
         <h1>Mount Glory School</h1>
+      </main> */}
+
+      <main className="z-50 h-screen w-full bg-white">
+        <div className="container flex size-full flex-col items-center justify-center gap-4 text-center">
+          <img
+            src="https://mntglory.saksham.edu.np/icon/logo.svg"
+            alt="Mount Glory School"
+            className="size-48 object-contain"
+          />
+          <h1 className="font-luxury mt-12 text-5xl">Mount Glory School</h1>
+
+          <p className="max-w-3xl">
+            Mount Glory excels in mentoring the current generation through a
+            tech-friendly, behavioral, and theoretical platform, fostering a
+            morale-driven environment. Join us for an unrivaled learning
+            experience.
+          </p>
+        </div>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        // luxury: ['"Gold Badge"', 'Georgia', 'Cambria', 'serif'],
-        // title: ['"Bourbon St"', 'Georgia', 'Cambria', 'serif'],
-        // kalnia: [
-        //   '"Kalnia"',
-        //   'ui-serif',
-        //   'Georgia',
-        //   'Cambria',
-        //   'Times New Roman',
-        //   'Times',
-        //   'serif',
-        // ],
-        // 'inria-sans': [
-        //   '"Inria Sans"',
-        //   'ui-sans-serif',
-        //   'system-ui',
-        //   'sans-serif',
-        // ],
-        // 'pt-sans': [
-        //   '"PT Sans Narrow"',
-        //   'ui-sans-serif',
-        //   'system-ui',
-        //   'sans-serif',
-        // ],
+        title: ['"Purna Title"', 'Georgia', 'Cambria', 'serif'],
+        body: ['"Kanit"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       },
       colors: {
         dark: '#0a0a0a',
